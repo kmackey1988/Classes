57a0715b2e2afd406c2c27ba85bc0b10b4de1b16
kmackey1988 <73615784+kmackey1988@users.noreply.github.com>
2022-05-21 00:29:48 -0400

    Add files via upload

---
+++ applied programing/GoGirlFighter/main.lua
@@ -0,0 +1,22 @@
+--kimberly mackey
+--04/10/2021
+
+function love.load()
+    love.graphics.setDefaultFilter("nearest", "nearest")
+    require('dependencies')
+    psFont = love.graphics.newFont("PressStart2P.ttf", 8)
+    love.graphics.setFont(psFont)
+
+    background = love.graphics.newImage('assets/graphics/background-4.png')
+
+end
+
+function love.update()
+
+end
+
+function love.draw()
+
+end
+
+
